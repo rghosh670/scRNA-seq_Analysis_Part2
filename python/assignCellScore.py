1d90bb4dd21104c88cd3cf4c040d5c24b7c15ffe
Rohit Ghosh <rohitghosh101@gmail.com>
2020-11-30 21:14:14 -0800

    updated clusters

--- python/assignCellScore.py
+++ python/assignCellScore.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-cellNames = pd.read_csv('data/t6.csv',index_col=[0], usecols=[0]).index.tolist()
+cellNames = pd.read_csv('data/rawReadsWithCellNames.csv',index_col=[0], usecols=[0]).index.tolist()
 cellScore = pd.DataFrame(index = cellNames)
 
-cellSum = pd.read_csv('data/t6sum.csv')['sum'].tolist()
+cellSum = pd.read_csv('data/sumReads.csv')['sum'].tolist()
 
-for j in range(15):
-    f = open('leidenClusters/cluster' + str(j) + '.txt', 'r')
+for j in range(16):
+    f = open('wardClusters/cluster' + str(j) + '.txt', 'r')
     genes = f.read().splitlines()
     genes.append('cell')
     f.close()
 
-    eset = pd.read_csv('data/t6.csv',index_col=[0], usecols=genes)
+    eset = pd.read_csv('data/rawReadsWithCellNames.csv',index_col=[0], usecols=genes)
 
     sum = eset.sum(axis = 1).tolist()
     scores = []
@@ -21,5 +21,5 @@ for j in range(15):
 
     cellScore['cluster' + str(j)] = scores
 
-cellScore.to_csv('leidenClusters/cellBinLeidenScore.csv')
+cellScore.to_csv('wardClusters/wardCellBinScore.csv')
 # exec(open('python/sendEmail.py').read())
--- python/parseLeiden.py
+++ python/parseLeiden.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-leiden = pd.read_csv('leiden2.csv', index_col = [0])
+leiden = pd.read_csv('data/leiden2.csv', index_col = [0])
 genes = leiden.index.tolist()
 clusters = leiden['leiden'].tolist()
-
 
 for index, value in enumerate(clusters):
     f = open('leidenClusters/cluster' + str(value) + '.txt', 'a+')
