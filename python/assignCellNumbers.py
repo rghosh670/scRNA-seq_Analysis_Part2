17a7fd2f2e3d2bfabe05823de2d028f82686f05e
rghosh670 <36284491+rghosh670@users.noreply.github.com>
2020-12-28 16:52:49 -0800

    DESingle stuff

--- python/assignCellNumbers.py
+++ python/assignCellNumbers.py
@@ -1,19 +1,21 @@
 import pandas as pd
-import importlib
+import statistics
 from cellNames import getCellName
 
 NUM_CELLS = 37148
+MEDIAN_TIME = 405.0
 
-cells = []
-for i in range(NUM_CELLS):
-    cells.append(getCellName(i))
+f = open('data/usefulTimes.txt', 'r')
+timeList = f.read().splitlines()
+timeList = [int(i) for i in timeList]
+f.close()
 
-result = pd.factorize(cells)[0].tolist()
-result = [int(i) + 1 for i in result]
+result_list = []
+for i in timeList:
+    result_list.append(1 if i <= MEDIAN_TIME else 2)
 
-with open('data/factorizedCellList', 'w') as f: # write out results to text file
-    for item in result:
+with open('group.txt', 'w') as f: # write out results to text file
+    for item in result_list:
         f.write("%s\n" % item)
 
 f.close()
-
